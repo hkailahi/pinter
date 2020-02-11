b661216f3d9250a6e4d681c5c2feac750602b746
Heneli Kailahi <heneli@kailahi.com>
2020-02-10 23:53:52 -0500

    Furst

---
+++ pinter/src/Main.hs
@@ -0,0 +1,7 @@
+module Main where
+
+import BasicPrelude
+
+main :: IO ()
+main = do
+  putStrLn "hello world"
---
+++ pinter/src/Pinter/Group.hs
@@ -0,0 +1,40 @@
+module Pinter.Group where
+
+import BasicPrelude
+
+import Data.Monoid (Product (Product, getProduct), Sum (Sum, getSum))
+
+-- |A type with an operation (<>) which is associative, has a neutral element, and for which each
+-- element has an inverse.
+-- 
+-- Laws:
+-- * Associativity
+--   * `<>` is associative
+-- * Identity
+--   * There is an element `e` such that `a <> e = a` and `e <> a = a` for every element `a` in `G`
+-- * Inverse
+--   * For every element `a` in `G`, there is and element `a^-1` such that `a <> a^-1 = e` and
+--   `a <> a^-1 = e`
+class Monoid m => Group m where
+  invert :: m -> m
+
+instance Num a => Group (Sum a) where
+  invert = Sum . negate . getSum
+
+instance Num a => Group (Product a) where
+  invert = Product . negate . getProduct
+
+-- A communative group. Named after the mathematician Niels Abel, who was a pioneer in the study of
+-- groups. Matrix multiplication is an example of an abelian group.
+-- 
+-- -- Laws:
+-- * Associativity
+--   * `<>` is associative
+-- * Identity
+--   * There is an element `e` such that `a * e = a` and `e * a = a` for every element `a` in `G`
+-- * Inverse
+--   * For every element `a` in `G`, there is and element `a^-1` such that `a <> a^-1 = e` and
+--   `a <> a^-1 = e`
+-- * Communativity
+--   * `a <> b == b <> a`
+class Group g => AbelianGroup g
